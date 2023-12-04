89631aafb81a84e7e233137b91e2e6ead6a17aa3
annahiggins1 <122704087+annahiggins1@users.noreply.github.com>
2023-12-04 18:25:48 -0500

    putting everything together in app + descriptions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,65 +1,51 @@
-import { react } from react;
-import { supabase } from './supabaseClient';
-import Posts from "../Posts";
-import AddPost from "../AddPost";
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Posts from "./Posts";
+import AddPost from "./AddPost";
+import { supabase } from './supabase';
 
 function App() {
-    const[msgs, setMsgs] = useState([]);
-    const[msg, setMsg] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  const [msg, setMsg] = useState("");
 
-    //gets messages from the database
-    async function getMsgs() {
-        try {
-            const { data, error } = await supabase
-            .from('Messages') //getting data from the messages table
-            .select('*'); //taking all the data from the table
-            if (error) throw error;
-            window.location.reload();
-        } catch (error) {
-            alert(error); // alerts the window if there's an error
-        }
+  useEffect(() => {
+    getMsgs();
+  }, []);
+
+
+
+  // gets messages from the database
+  async function getMsgs() {
+    try {
+      const { data, error } = await supabase
+        .from('Messages') // getting data from the messages table
+        .select('*'); // taking all the data from the table
+      if (error) throw error;
+      console.log("Data: ", data);
+      if (data != null) {
+        setMsgs(data);
+      }
+    } catch (error) {
+      alert(error); // alerts the window if there's an error
     }
+  }
 
-    //adds the given message to the database
-    async function addMsg(message) {
-        //enforces the message to have a length less than 128
-        if (length(message) > 128) {
-            alert("Message must be less than 128 characters");
-            return;
-        }
-
-        //tries to insert the message into the database
-        try {
-            const { data, error } = await supabase
-            .from('Messages')
-            .insert([
-                { message: message },
-            ]);
-            if (error) throw error;
-            window.location.reload();
-        } 
-        //if fails, alerts window
-        catch (error) {
-            alert(error);
-        }
-    }
-
-    return (
-        <div>
-            <div>
-                <AddPost //displays the posting box
-                msg={msg} 
-                setMsg={setMsg}> </AddPost>
-            </div>
-            <div>
-                <Posts //displays post history
-                msgs={msgs}> 
-                 </Posts>
-            </div>
-        </div>
-    );
-
-
-
+  return (
+    <div className="app-container">
+      <div className="centered-container">
+        <AddPost // displays the posting box
+          messageContents={msg}
+          setMessage={setMsg}
+        />
+      </div>
+      <div className="centered-container">
+        <Posts // displays post history
+          messages={msgs}
+          getMessages={getMsgs}
+        />
+      </div>
+    </div>
+  );
 }
+
+export default App;
