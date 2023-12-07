194c71ad9a41fa7cfc478e9b94b1368d2c7ea70b
annahiggins1 <122704087+annahiggins1@users.noreply.github.com>
2023-12-07 15:33:51 -0500

    add light/dark mode button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css";
-import Posts from "./Posts";
-import AddPost from "./AddPost";
+import Posts from "./components/Posts";
+import AddPost from "./components/AddPost";
+import ThemeButton from "./components/ThemeButton";
 import { supabase } from './supabase';
 
 function App() {
   const [msgs, setMsgs] = useState([]);
   const [msg, setMsg] = useState("");
+  const [theme, setTheme] = useState('light');
 
   useEffect(() => {
     getMsgs();
   }, []);
 
 
-
-  // gets messages from the database
   async function getMsgs() {
     try {
       const { data, error } = await supabase
-        .from('Messages') // getting data from the messages table
-        .select('*'); // taking all the data from the table
+        .from('Messages')
+        .select('*');
       if (error) throw error;
       console.log("Data: ", data);
       if (data != null) {
         setMsgs(data);
       }
     } catch (error) {
-      alert(error); // alerts the window if there's an error
+      alert(error);
     }
   }
 
   return (
-    <div className="app-container">
+    <div className="app-container" data-theme={theme}>
+      <h1 className="header">Message Board</h1>
       <div className="centered-container">
-        <AddPost // displays the posting box
+        <ThemeButton setTheme={setTheme}/>
+      </div>
+      <div className="centered-container">
+        <AddPost
           messageContents={msg}
           setMessage={setMsg}
         />
       </div>
       <div className="centered-container">
-        <Posts // displays post history
+        <Posts
           messages={msgs}
           getMessages={getMsgs}
         />
